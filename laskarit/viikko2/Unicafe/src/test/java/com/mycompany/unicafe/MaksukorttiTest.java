3dc673c2b3e29f1b874a24687a28555a75f1fd3d
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-03-21 12:44:21 +0200

    unicafe testailua

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,39 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein(){
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldonKasvatusToimii(){
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiVaheneJosRahaaEiTarpeeksi(){
+        kortti.otaRahaa(20);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikein(){
+        kortti.otaRahaa(10);
+        assertEquals("saldo: 0.0", kortti.toString());
+    }
+    
+    @Test
+    public void palauttaaTrueKunRahatRiittaa(){
+        boolean arvo = kortti.otaRahaa(10);
+        assertEquals(true, arvo);
+    }
+    
+    @Test 
+    public void palauttaaFalseKunRahaEiRiita(){
+        boolean arvo = kortti.otaRahaa(100);
+        assertEquals(false, arvo);
+    }
 }
