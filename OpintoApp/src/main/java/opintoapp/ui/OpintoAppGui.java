2f6f8f7da9e43d0150f706d8c24a49fd224b9281
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-03-26 20:43:01 +0300

    fxml ui basics

--- OpintoApp/src/main/java/opintoapp/ui/OpintoAppGui.java
+++ OpintoApp/src/main/java/opintoapp/ui/OpintoAppGui.java
@@ -2,13 +2,22 @@
 package opintoapp.ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class OpintoAppGui extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Hello world");
+        FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+        Parent root = loginLoader.load();
+        
+        Scene scene = new Scene(root, 300, 275);
+        
+        stage.setTitle("hello fxml world");
+        stage.setScene(scene);
         stage.show();
     }
     
