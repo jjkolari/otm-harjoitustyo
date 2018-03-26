4e416a455b9c3222f7b6c5f059f77bd857bd1ece
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-03-26 14:14:50 +0300

    1st javafx test

---
+++ OpintoApp/src/main/java/opintoapp/ui/OpintoAppGui.java
@@ -0,0 +1,25 @@
+
+package opintoapp.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OpintoAppGui extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello world");
+        stage.show();
+    }
+    
+    public static void main(String[] args) {
+        
+        launch(args);
+        
+        
+    }
+    
+    
+    
+    
+}
