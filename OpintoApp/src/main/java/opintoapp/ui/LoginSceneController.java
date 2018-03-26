9141e495d727cf639ab0f1a318f44b4d3adf3ecc
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-03-26 21:14:43 +0300

    button action controller added

---
+++ OpintoApp/src/main/java/opintoapp/ui/LoginSceneController.java
@@ -0,0 +1,15 @@
+
+package opintoapp.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class LoginSceneController {
+    
+    @FXML private Text actiontarget;
+    
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
+}
