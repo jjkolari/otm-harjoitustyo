35efad3ac0fdde66d5ad346910e5cc1153195f79
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-04-22 14:03:18 +0300

    database-class creates db on 1st launch if doesnt exist, db added to .gitignore

--- OpintoApp/src/main/java/opintoapp/dao/Database.java
+++ OpintoApp/src/main/java/opintoapp/dao/Database.java
@@ -8,6 +8,28 @@ public class Database {
 
     public Database(String databaseAdress) throws ClassNotFoundException {
         this.databaseAdress = databaseAdress;
+        try {
+            Connection conn = DriverManager.getConnection(databaseAdress);
+            Statement stmt = conn.createStatement();
+            stmt.execute("CREATE TABLE IF NOT EXISTS"
+                    + " User (id integer PRIMARY KEY,"
+                    + " username varchar(200),"
+                    + " name varchar(200),"
+                    + " password varchar(200)"
+                    + ");");
+            
+            stmt.execute("CREATE TABLE IF NOT EXISTS"
+                    + " Course (id integer PRIMARY KEY,"
+                    + " name varchar(200),"
+                    + " credits integer,"
+                    + " grade integer,"
+                    + " user_username varchar(200),"
+                    + " FOREIGN KEY (user_username) REFERENCES User(username)"
+                    + ");");
+            
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public Connection getConnection() throws SQLException {
